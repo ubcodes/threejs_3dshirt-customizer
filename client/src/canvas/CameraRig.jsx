17522edd497269c798240bf47499bce72cdcbd68
UB30-4 <101749091+UB30-4@users.noreply.github.com>
2023-05-18 18:55:31 +0100

    update: gitignore

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -2,9 +2,7 @@ import React, { useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { easing } from 'maath';
 import { useSnapshot } from 'valtio';
-
 import state from '../store';
-
 
 const CameraRig = ({ children }) => {
     const group = useRef();
